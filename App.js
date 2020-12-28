3be5db34cade6f192f4a5547e26b630bf769d3e2
emmanuelferreira <emmanuel.p.ferreira@gmail.com>
2020-12-28 19:32:37 +0100

    reducers and store persisted via redux-persit library implemented

--- App.js
+++ App.js
@@ -2,14 +2,20 @@ import React from 'react';
 import Navigation from './navigation/Navigation';
 import { Provider } from 'react-redux';
 import store from './store/configureStore';
+import { persistStore } from 'redux-persist'
+import { PersistGate } from 'redux-persist/es/integration/react'
+
 
 
 export default class App extends React.Component {
 
   render(){
+    let persistor = persistStore(store)
     return (
       <Provider store={store}>
-        <Navigation />
+        <PersistGate persistor={persistor}>
+          <Navigation />
+        </PersistGate>
       </Provider>
     )
   }
--- store/configureStore.js
+++ store/configureStore.js
@@ -1,7 +1,16 @@
 import {createStore, combineReducers} from 'redux'
+import { persistCombineReducers } from 'redux-persist'
+import AsyncStorage from '@react-native-community/async-storage'
+
+
 import favoritesMovies from './reducers/favoritesReducer'
 import avatarReducer from './reducers/avatarReducer'
 
-const store = createStore(combineReducers({favoritesMovies, avatarReducer}))
+const rootPersistConfig = {
+  key: 'root',
+  storage: AsyncStorage
+}
+
+const store = createStore(persistCombineReducers(rootPersistConfig, {favoritesMovies, avatarReducer}))
 
 export default store
